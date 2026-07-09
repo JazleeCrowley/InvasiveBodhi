9a7c83652aa003bacf0faa82c291af1797c847c0
JazleeCrowley <jazlee.crowley5@gmail.com>
2026-07-09 16:09:56 -0700

    created a supplemenatry occurrence data sheet

---
+++ code/Manuscript tables/Supp_Table.r
@@ -0,0 +1,42 @@
+Create a supplementary table for all Bodhi tree occurrences in Hawaii
+library(sf)
+library(readr)
+install.packages("sf")
+install.packages("readr")
+
+# Unzip KMZ
+unzip(
+  "/Users/jazleecrowley/Git/InvasiveBodhi/data/raw/HI_Occurrences.kmz",
+  exdir = "/Users/jazleecrowley/Git/InvasiveBodhi/data/raw/HI_Occurrences_unzipped"
+)
+
+# See what files were extracted
+list.files(
+  "/Users/jazleecrowley/Git/InvasiveBodhi/data/raw/HI_Occurrences_unzipped",
+  recursive = TRUE,
+  full.names = TRUE
+)
+
+# Read KML
+trees <- st_read(
+  "/Users/jazleecrowley/Git/InvasiveBodhi/data/raw/HI_Occurrences_unzipped/doc.kml"
+)
+
+# Extract coordinates
+coords <- st_coordinates(trees)
+
+trees$longitude <- coords[,1]
+trees$latitude <- coords[,2]
+
+# Remove geometry
+trees_csv <- st_drop_geometry(trees)
+
+# Save CSV
+write_csv(trees_csv, "HI_Occurrences.csv")
+setwd("/Users/jazleecrowley/Git/InvasiveBodhi")
+getwd()
+list.files()
+write_csv(
+  trees_csv,
+  "/Users/jazleecrowley/Desktop/BodhiTrees.csv"
+)
